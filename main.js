b249dc414fca88ef8073f96426fc7c3cbe08f68d
ahmadsamerlego@gmail.com <ahmadsamerlego@gmail.com>
2022-09-20 23:52:51 +0200

    hhh

--- main.js
+++ main.js
@@ -1,280 +1,81 @@
-// //javascript.js
-// //set map options
-// var myLatLng = { lat: 38.3460, lng: -0.4907 };
-// var mapOptions = {
-//     center: myLatLng,
-//     zoom: 7,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
 
-// };
-
-// //create map
-// var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
-// //create a DirectionsService object to use the route method and get a result for our request
-// var directionsService = new google.maps.DirectionsService();
-
-// //create a DirectionsRenderer object which we will use to display the route
-// var directionsDisplay = new google.maps.DirectionsRenderer();
-
-// //bind the DirectionsRenderer to the map
-// directionsDisplay.setMap(map);
-
-//   const trafficLayer = new google.maps.TrafficLayer();
-
-//   trafficLayer.setMap(map);
-
-// //define calcRoute function
-// function calcRoute() {
-//     //create request
-//     var request = {
-//         origin: document.getElementById("from").value,
-//         destination: document.getElementById("to").value,
-//         travelMode: google.maps.TravelMode.DRIVING, 
-//         travelMode: google.maps.TravelMode.BYCYCLING, 
-//         travelMode: google.maps.TravelMode.WALKING,
-//         travelMode: google.maps.TravelMode.TRANSIT, 
-//         unitSystem: google.maps.UnitSystem.IMPERIAL
-//     }
-
-//     //pass the request to the route method
-//     directionsService.route(request, function (result, status) {
-//         if (status == google.maps.DirectionsStatus.OK) {
-
-//             //Get distance and time
-//             const output = document.querySelector('#output');
-//             output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
-
-//             //display route
-//             directionsDisplay.setDirections(result);
-//         } else {
-//             //delete route from map
-//             directionsDisplay.setDirections({ routes: [] });
-//             //center map in London
-//             map.setCenter(myLatLng);
-
-//             //show error message
-//             output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
-//         }
-//     });
-
-// }
-
-
-
-// //create autocomplete objects for all inputs
-// var options = {
-//     types: ['(cities)']
-// }
-
-// var input1 = document.getElementById("from");
-// var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
-// var input2 = document.getElementById("to");
-// var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
-
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script
-// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      mapTypeControl: false,
-      center: { lat: -33.8688, lng: 151.2195 },
-      zoom: 13,
-    });
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: {
+      lat: 41.85,
+      lng: -87.65
+    }
+  });
+  directionsDisplay.setMap(map);
   
-    new AutocompleteDirectionsHandler(map);
+  const start = document.getElementById('start');
+  const end = document.getElementById('end');
+  const way1 = document.getElementById('waypoints1');
+  const way2 = document.getElementById('waypoints2');
+  const way3 = document.getElementById('waypoints3');
+  const way4 = document.getElementById('waypoints4');
+  new google.maps.places.Autocomplete(start);
+  new google.maps.places.Autocomplete(end);
+  new google.maps.places.Autocomplete(way1);
+  new google.maps.places.Autocomplete(way2);
+  new google.maps.places.Autocomplete(way3);
+  new google.maps.places.Autocomplete(way4);
+
+
+
+  document.getElementById('submit').addEventListener('click', function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+}
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var waypts = [];
+  var checkboxArray = document.getElementsByClassName('waypoints');
+  for (var i = 0; i < checkboxArray.length; i++) {
+    var address = checkboxArray[i].value;
+    if (address !== '') {
+      waypts.push({
+        location: address,
+        stopover: true
+      });
+    }
   }
-  
-  class AutocompleteDirectionsHandler {
-    map;
-    originPlaceId;
-    destinationPlaceId;
-    travelMode;
-    directionsService;
-    directionsRenderer;
-    constructor(map) {
-      this.map = map;
-      this.originPlaceId = "";
-      this.destinationPlaceId = "";
-      this.travelMode = google.maps.TravelMode.WALKING;
-      this.directionsService = new google.maps.DirectionsService();
-      this.directionsRenderer = new google.maps.DirectionsRenderer();
-      this.directionsRenderer.setMap(map);
-  
-      const originInput = document.getElementById("origin-input");
-      const destinationInput = document.getElementById("destination-input");
-      const modeSelector = document.getElementById("mode-selector");
-      // Specify just the place data fields that you need.
-      const originAutocomplete = new google.maps.places.Autocomplete(
-        originInput,
-        { fields: ["place_id"] }
-      );
-      // Specify just the place data fields that you need.
-      const destinationAutocomplete = new google.maps.places.Autocomplete(
-        destinationInput,
-        { fields: ["place_id"] }
-      );
-  
-      this.setupClickListener(
-        "changemode-walking",
-        google.maps.TravelMode.WALKING
-      );
-      this.setupClickListener(
-        "changemode-transit",
-        google.maps.TravelMode.TRANSIT
-      );
-      this.setupClickListener(
-        "changemode-driving",
-        google.maps.TravelMode.DRIVING
-      );
-      this.setupClickListener(
-        "changemode-byicycle",
-        google.maps.TravelMode.BICYCLING
-      );
-      this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-      this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-        destinationInput
-      );
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-    }
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
-    setupClickListener(id, mode) {
-      const radioButton = document.getElementById(id);
-  
-      radioButton.addEventListener("click", () => {
-        this.travelMode = mode;
-        this.route();
-        this.calcRoute()
-      });
-    }
-    setupPlaceChangedListener(autocomplete, mode) {
-      autocomplete.bindTo("bounds", this.map);
-      autocomplete.addListener("place_changed", () => {
-        const place = autocomplete.getPlace();
-  
-        if (!place.place_id) {
-          window.alert("Please select an option from the dropdown list.");
-          return;
-        }
-  
-        if (mode === "ORIG") {
-          this.originPlaceId = place.place_id;
-        } else {
-          this.destinationPlaceId = place.place_id;
-        }
-  
-        this.route();
-        this.calcRoute()
-      });
-    }
-    route() {
-      if (!this.originPlaceId || !this.destinationPlaceId) {
-        return;
-      }
-  
-      const me = this;
-  
-      this.directionsService.route(
-        {
-          origin: { placeId: this.originPlaceId },
-          destination: { placeId: this.destinationPlaceId },
-          travelMode: this.travelMode,
-        },
-        (response, status) => {
-          if (status === "OK") {
-            me.directionsRenderer.setDirections(response);
-          } else {
-            window.alert("Directions request failed due to " + status);
-          }
-        }
-      );
-    }
+  const selectedMode = document.getElementById("mode").value;
 
-    calcRoute() {
-        //create request
-        var request = {
-            origin: document.getElementById("origin-input").value,
-            destination: document.getElementById("destination-input").value,
-            travelMode: this.travelMode,
-            unitSystem: google.maps.UnitSystem.IMPERIAL
-        }
-    
-        //pass the request to the route method
-        this.directionsService.route(request, function (result, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-    
-                //Get distance and time
-                const output = document.querySelector('#output');
-                output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("origin-input").value + ".<br />To: " + document.getElementById("destination-input").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
-    
-                //display route
-                this.directionsRenderer.setDirections(result);
-
-                alert('done')
-            } else {
-                //delete route from map
-                this.directionsRenderer.setDirections({ routes: [] });
-                //center map in London
-                map.setCenter(myLatLng);
-    
-                //show error message
-                output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
-            }
-        });
-    
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    waypoints: waypts,
+    optimizeWaypoints: true,
+    travelMode: google.maps.TravelMode[selectedMode],
+  },  
+function(response, status) {
+  if (status === 'OK') {
+    directionsDisplay.setDirections(response);
+    var route = response.routes[0];
+    var summaryPanel = document.getElementById('directions-panel');
+    summaryPanel.innerHTML = '';
+    // For each route, display summary information.
+    for (var i = 0; i < route.legs.length; i++) {
+      var routeSegment = i + 1;
+      summaryPanel.innerHTML += '<b>Route level: ' + routeSegment +
+        '</b><br>'; 
+        
+      summaryPanel.innerHTML +='from'+ ' :'+route.legs[i].start_address + '<br>'+' to :' 
+      
+      summaryPanel.innerHTML += route.legs[i].end_address + '<br><br>';
+      summaryPanel.innerHTML += 'distance in miles :' +route.legs[i].distance.text + '<br><br>';
+      summaryPanel.innerHTML += 'time in houres :' +route.legs[i].duration.text + '<br><br><hr>';
     }
-    }
+  } else {
+    window.alert('Directions request failed due to ' + status);
+  }
+  }
+  );
+}
 
-
-
-  
-  window.initMap = initMap;
-  
+google.maps.event.addDomListener(window, "load", initMap);
